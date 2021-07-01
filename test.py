2a18b1c9c7fc8b17494c6a1da09462734ed50b12
alexlam <alexlam1994@live.com>
2021-07-01 11:56:19 +1000

    updated file

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
-prompt = "Give me an integer: "
-def get_int():
-    return int(input(prompt)) 
-x = get_int()
-y = get_int()
-def calc_hcf (a,b):
-    if b==0:
-        return a
-    else:
-        return calc_hcf(b, a%b)
-print(calc_hcf(x,y))
+def func5(a):
+    print("in func5 a = ", a)
+    b = 100 + a
+    d = 2 * a
+    print("in func5 b = ", b)
+    print("in func5 d = ", d)
+    print("in func5 c = ", c)
+    return b + 10
+          
+a = 10
+b = 15
+c = 25
+c = func5(b) 
+# 
+print("a = ", a)
+print("b = ", b) 
+print("c = ", c) 
+# a = 10
+# b = 110
+# d = 20 
+
+#print("d = ", d) # d is not defined
